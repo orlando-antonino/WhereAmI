8e689ec1cae4e1083ee3482b83ba9b36d3ae1a0f
justBorn <alessandro.francia@gmail.com>
2013-10-13 03:45:21 +0200

    Update Const.java

--- WhereAmIprj/src/com/whereami/Const.java
+++ WhereAmIprj/src/com/whereami/Const.java
@@ -1,5 +1,7 @@
 package com.whereami;
 
 public class Const {
-	 public static boolean _DEBUG = true; 
+	 public static boolean _DEBUG = false;
+	 public static String _SECRET_WORD = "whereIs"; 
+
 }
